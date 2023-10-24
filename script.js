379e6c11903e34ebe3a91467a1c5a6b8a06330ca
Eduardo Secco <132023142+dudssecco@users.noreply.github.com>
2023-10-24 14:25:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,159 @@
+    // Douglas
+function AddAcertoDouglas(){
+
+    let inputAcertoDouglas = Number(document.getElementById('iadouglas').value)
+    inputAcertoDouglas = parseInt(inputAcertoDouglas + 1)
+    document.getElementById('iadouglas').value = inputAcertoDouglas
+}
+
+function RemoveAcertoDouglas(){
+
+    let inputAcertoDouglas = Number(document.getElementById('iadouglas').value)
+    inputAcertoDouglas = parseInt(inputAcertoDouglas - 1)
+    document.getElementById('iadouglas').value = inputAcertoDouglas
+}
+
+function AddErroDouglas(){
+
+    let inputErroDouglas = Number(document.getElementById('iedouglas').value)
+    inputErroDouglas = parseInt(inputErroDouglas + 1)
+    document.getElementById('iedouglas').value = inputErroDouglas
+}
+
+
+    //Derick
+function AddAcertoDerick(){
+
+    let inputAcertoDerick = Number(document.getElementById('iaderick').value)
+    inputAcertoDerick = parseInt(inputAcertoDerick + 1)
+    document.getElementById('iaderick').value = inputAcertoDerick
+}
+    
+function RemoveAcertoDerick(){
+    
+    let inputAcertoDerick = Number(document.getElementById('iaderick').value)
+    inputAcertoDerick = parseInt(inputAcertoDerick - 1)
+    document.getElementById('iaderick').value = inputAcertoDerick
+}
+    
+function AddErroDerick(){
+    
+    let inputErroDerick = Number(document.getElementById('iederick').value)
+    inputErroDerick = parseInt(inputErroDerick + 1)
+    document.getElementById('iederick').value = inputErroDerick
+}
+
+
+    //Rafa
+function AddAcertoRafa(){
+
+    let inputAcertoRafa = Number(document.getElementById('iarafa').value)
+    inputAcertoRafa = parseInt(inputAcertoRafa + 1)
+    document.getElementById('iarafa').value = inputAcertoRafa
+}
+        
+function RemoveAcertoRafa(){
+        
+    let inputAcertoRafa = Number(document.getElementById('iarafa').value)
+    inputAcertoRafa = parseInt(inputAcertoRafa - 1)
+    document.getElementById('iarafa').value = inputAcertoRafa
+}
+        
+function AddErroRafa(){
+        
+    let inputErroRafa = Number(document.getElementById('ierafa').value)
+    inputErroRafa = parseInt(inputErroRafa + 1)
+    document.getElementById('ierafa').value = inputErroRafa
+}
+
+
+    //Raul
+function AddAcertoRaul(){
+
+    let inputAcertoRaul = Number(document.getElementById('iaraul').value)
+    inputAcertoRaul = parseInt(inputAcertoRaul + 1)
+    document.getElementById('iaraul').value = inputAcertoRaul
+}
+            
+function RemoveAcertoRaul(){
+            
+    let inputAcertoRaul = Number(document.getElementById('iaraul').value)
+    inputAcertoRaul = parseInt(inputAcertoRaul - 1)
+    document.getElementById('iaraul').value = inputAcertoRaul
+}
+            
+function AddErroRaul(){
+            
+    let inputErroRaul = Number(document.getElementById('ieraul').value)
+    inputErroRaul = parseInt(inputErroRaul + 1)
+    document.getElementById('ieraul').value = inputErroRaul
+}
+
+
+    //Robson
+function AddAcertoRobson(){
+
+    let inputAcertoRobson = Number(document.getElementById('iarobson').value)
+    inputAcertoRobson = parseInt(inputAcertoRobson + 1)
+    document.getElementById('iarobson').value = inputAcertoRobson
+}
+            
+function RemoveAcertoRobson(){
+            
+    let inputAcertoRobson = Number(document.getElementById('iarobson').value)
+    inputAcertoRobson = parseInt(inputAcertoRobson - 1)
+    document.getElementById('iarobson').value = inputAcertoRobson
+}
+            
+function AddErroRobson(){
+            
+    let inputErroRobson = Number(document.getElementById('ierobson').value)
+    inputErroRobson = parseInt(inputErroRobson + 1)
+    document.getElementById('ierobson').value = inputErroRobson
+}
+
+
+    //Eduardo
+function AddAcertoEduardo(){
+
+    let inputAcertoEduardo = Number(document.getElementById('iaeduardo').value)
+    inputAcertoEduardo = parseInt(inputAcertoEduardo + 1)
+    document.getElementById('iaeduardo').value = inputAcertoEduardo
+}
+            
+function RemoveAcertoEduardo(){
+            
+    let inputAcertoEduardo = Number(document.getElementById('iaeduardo').value)
+    inputAcertoEduardo = parseInt(inputAcertoEduardo - 1)
+    document.getElementById('iaeduardo').value = inputAcertoEduardo
+}
+            
+function AddErroEduardo(){
+            
+    let inputErroEduardo = Number(document.getElementById('ieeduardo').value)
+    inputErroEduardo = parseInt(inputErroEduardo + 1)
+    document.getElementById('ieeduardo').value = inputErroEduardo
+}
+
+
+    //Gustavo
+function AddAcertoGustavo(){
+
+    let inputAcertoGustavo = Number(document.getElementById('iagustavo').value)
+    inputAcertoGustavo = parseInt(inputAcertoGustavo + 1)
+    document.getElementById('iagustavo').value = inputAcertoGustavo
+}
+            
+function RemoveAcertoGustavo(){
+            
+    let inputAcertoGustavo = Number(document.getElementById('iagustavo').value)
+    inputAcertoGustavo = parseInt(inputAcertoGustavo - 1)
+    document.getElementById('iagustavo').value = inputAcertoGustavo
+}
+            
+function AddErroGustavo(){
+            
+    let inputErroGustavo = Number(document.getElementById('iegustavo').value)
+    inputErroGustavo = parseInt(inputErroGustavo + 1)
+    document.getElementById('iegustavo').value = inputErroGustavo
+}
