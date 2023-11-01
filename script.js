49583aa34ce2a56b6844b37ede0dc2ed12ba2673
Eduardo Secco <132023142+dudssecco@users.noreply.github.com>
2023-11-01 09:49:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,159 +1,33 @@
-    // Douglas
-function AddAcertoDouglas(){
-
-    let inputAcertoDouglas = Number(document.getElementById('iadouglas').value)
-    inputAcertoDouglas = parseInt(inputAcertoDouglas + 1)
-    document.getElementById('iadouglas').value = inputAcertoDouglas
-}
-
-function RemoveAcertoDouglas(){
-
-    let inputAcertoDouglas = Number(document.getElementById('iadouglas').value)
-    inputAcertoDouglas = parseInt(inputAcertoDouglas - 1)
-    document.getElementById('iadouglas').value = inputAcertoDouglas
-}
-
-function AddErroDouglas(){
-
-    let inputErroDouglas = Number(document.getElementById('iedouglas').value)
-    inputErroDouglas = parseInt(inputErroDouglas + 1)
-    document.getElementById('iedouglas').value = inputErroDouglas
-}
-
-
-    //Derick
-function AddAcertoDerick(){
-
-    let inputAcertoDerick = Number(document.getElementById('iaderick').value)
-    inputAcertoDerick = parseInt(inputAcertoDerick + 1)
-    document.getElementById('iaderick').value = inputAcertoDerick
-}
+let select = document.getElementById('idnome')
+let user
     
-function RemoveAcertoDerick(){
+select.addEventListener('change', function(){
     
-    let inputAcertoDerick = Number(document.getElementById('iaderick').value)
-    inputAcertoDerick = parseInt(inputAcertoDerick - 1)
-    document.getElementById('iaderick').value = inputAcertoDerick
-}
+    user = select.value
     
-function AddErroDerick(){
-    
-    let inputErroDerick = Number(document.getElementById('iederick').value)
-    inputErroDerick = parseInt(inputErroDerick + 1)
-    document.getElementById('iederick').value = inputErroDerick
+})
+
+
+function addError(){
+
+    openScore()
+   
 }
 
 
-    //Rafa
-function AddAcertoRafa(){
+function openScore(){
 
-    let inputAcertoRafa = Number(document.getElementById('iarafa').value)
-    inputAcertoRafa = parseInt(inputAcertoRafa + 1)
-    document.getElementById('iarafa').value = inputAcertoRafa
-}
-        
-function RemoveAcertoRafa(){
-        
-    let inputAcertoRafa = Number(document.getElementById('iarafa').value)
-    inputAcertoRafa = parseInt(inputAcertoRafa - 1)
-    document.getElementById('iarafa').value = inputAcertoRafa
-}
-        
-function AddErroRafa(){
-        
-    let inputErroRafa = Number(document.getElementById('ierafa').value)
-    inputErroRafa = parseInt(inputErroRafa + 1)
-    document.getElementById('ierafa').value = inputErroRafa
+    cadastro.style.display = 'none'
+    score.style.display = 'block'
+
 }
 
+function openCadastro(){
 
-    //Raul
-function AddAcertoRaul(){
+    score.style.display = 'none'
+    cadastro.style.display = 'block'
 
-    let inputAcertoRaul = Number(document.getElementById('iaraul').value)
-    inputAcertoRaul = parseInt(inputAcertoRaul + 1)
-    document.getElementById('iaraul').value = inputAcertoRaul
 }
-            
-function RemoveAcertoRaul(){
-            
-    let inputAcertoRaul = Number(document.getElementById('iaraul').value)
-    inputAcertoRaul = parseInt(inputAcertoRaul - 1)
-    document.getElementById('iaraul').value = inputAcertoRaul
-}
-            
-function AddErroRaul(){
-            
-    let inputErroRaul = Number(document.getElementById('ieraul').value)
-    inputErroRaul = parseInt(inputErroRaul + 1)
-    document.getElementById('ieraul').value = inputErroRaul
-}
+   
 
 
-    //Robson
-function AddAcertoRobson(){
-
-    let inputAcertoRobson = Number(document.getElementById('iarobson').value)
-    inputAcertoRobson = parseInt(inputAcertoRobson + 1)
-    document.getElementById('iarobson').value = inputAcertoRobson
-}
-            
-function RemoveAcertoRobson(){
-            
-    let inputAcertoRobson = Number(document.getElementById('iarobson').value)
-    inputAcertoRobson = parseInt(inputAcertoRobson - 1)
-    document.getElementById('iarobson').value = inputAcertoRobson
-}
-            
-function AddErroRobson(){
-            
-    let inputErroRobson = Number(document.getElementById('ierobson').value)
-    inputErroRobson = parseInt(inputErroRobson + 1)
-    document.getElementById('ierobson').value = inputErroRobson
-}
-
-
-    //Eduardo
-function AddAcertoEduardo(){
-
-    let inputAcertoEduardo = Number(document.getElementById('iaeduardo').value)
-    inputAcertoEduardo = parseInt(inputAcertoEduardo + 1)
-    document.getElementById('iaeduardo').value = inputAcertoEduardo
-}
-            
-function RemoveAcertoEduardo(){
-            
-    let inputAcertoEduardo = Number(document.getElementById('iaeduardo').value)
-    inputAcertoEduardo = parseInt(inputAcertoEduardo - 1)
-    document.getElementById('iaeduardo').value = inputAcertoEduardo
-}
-            
-function AddErroEduardo(){
-            
-    let inputErroEduardo = Number(document.getElementById('ieeduardo').value)
-    inputErroEduardo = parseInt(inputErroEduardo + 1)
-    document.getElementById('ieeduardo').value = inputErroEduardo
-}
-
-
-    //Gustavo
-function AddAcertoGustavo(){
-
-    let inputAcertoGustavo = Number(document.getElementById('iagustavo').value)
-    inputAcertoGustavo = parseInt(inputAcertoGustavo + 1)
-    document.getElementById('iagustavo').value = inputAcertoGustavo
-}
-            
-function RemoveAcertoGustavo(){
-            
-    let inputAcertoGustavo = Number(document.getElementById('iagustavo').value)
-    inputAcertoGustavo = parseInt(inputAcertoGustavo - 1)
-    document.getElementById('iagustavo').value = inputAcertoGustavo
-}
-            
-function AddErroGustavo(){
-            
-    let inputErroGustavo = Number(document.getElementById('iegustavo').value)
-    inputErroGustavo = parseInt(inputErroGustavo + 1)
-    document.getElementById('iegustavo').value = inputErroGustavo
-}
